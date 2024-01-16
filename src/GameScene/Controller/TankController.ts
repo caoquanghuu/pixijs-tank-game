f4e8637ce862c70e6b3914e9a6008152d9b60eba
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-16 18:22:39 +0700

    add: tank controller class

--- src/GameScene/Controller/TankController.ts
+++ src/GameScene/Controller/TankController.ts
@@ -1,23 +1,45 @@
 import { Tank } from "../Objects/Tank";
-import { ObjectController } from "./ObjectController";
 
-export class TankController extend ObjectController {
+export class TankController {
     private _usingTanks: Tank[] = [];
     private _spawnTankTime: number;
+    private _playerTank: Tank;
 
+    constructor() {
+        // spawnTank every spawnTankTime
+
+        // create a player tank
+    }
+
+    /**
+     * spawn a enemy tank
+     */
     private spawnTank() {
         // get a Tank from TankPool then display it.
         // set Random Position and Direction for it.
     }
 
+    /**
+     * tank will die
+     * @param tank tank which will die
+     */
     private tankDie(tank: Tank) {
-        // check tank die is player or AI tank
-        // if player tank die return game over to game sense, if ene tank die then:
-        // check tank die in using tank, return it to tank pool and set it HP to 1.
+        /** check tank die is player or AI tank
+        if player tank die return game over to game sense, if ene tank die then:
+        check tank die in using tank, return it to tank pool and set it HP to 1.  */
     }
 
-    private tankMove(tank: Tank) {
+    /**
+     * handle move of tank when have collision
+     * @param tank tank which need to handle move
+     */
+    private tankHandleMove(tank: Tank) {
         // check collision is allow tank move, if have collision then set tank can not move.
         // if have no collision tank ll move normally.
+    }
+
+    /**method get tank list for check collision can access */
+    get usingTankList(): Tank[] {
+        return this._usingTanks;
     }
 }
