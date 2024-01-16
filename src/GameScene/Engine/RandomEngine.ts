0027ff03a8f4cf389d89b7a83154c7427a2bcc8c
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-16 18:23:36 +0700

    add: random engine class

--- src/GameScene/Engine/RandomEngine.ts
+++ src/GameScene/Engine/RandomEngine.ts
@@ -1,3 +1,25 @@
+import { Direction } from "../type";
+
 export class RandomEngine {
-    
+    private _direction: Direction;
+    private _directionChangeTime: number = 2000;
+
+    /**method to get direction from this engine */
+    public getDirection() {
+        return this._direction;
+    }
+
+    /**
+     * random get a direction from enum
+     */
+    private randomDirection() {
+        /**random to get direction from enum Direction */
+        /**assign direction to this direction */
+    }
+
+    public update(dt: number) {
+        /**set for the direction change time reduce to dt. */
+        /** if it = 0 then set randomDirection again to get new direction. */
+    }
+
 }
