c489b67c7f0d05e41d90ba4dee8f5315b275507a
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-16 18:21:24 +0700

    add: collision controller class

--- src/GameScene/Controller/CollisionController.ts
+++ src/GameScene/Controller/CollisionController.ts
@@ -1,5 +1,36 @@
-import { ObjectController } from "./ObjectController"
+import { Point } from "@pixi/core";
+import { BaseObject } from "../Objects/BaseObject";
 
-export class CollisionController{
+export class CollisionController {
+    /**position of tanks which be using on map */
+    private _tanksPosition: Point[] = [];
+    /**position of bullet which displaying on map */
+    private _bulletsPosition: Point[] = [];
+    /**position of environment displaying on map */
+    private _environmentsPosition: Point [] = [];
 
+    /**
+     * calculate distance of 2 object on map
+     * @param pos1 position of object 1
+     * @param pos2 position of object 2
+     */
+    private getDistanceOfTwoObject(pos1: Point, pos2: Point) {
+        /**calculate distance of 2 position */
+
+        /**return distance */
+    }
+
+    /**
+     * check collision of 2 object list
+     * @param object1 object list 1
+     * @param object2 object list 2
+     */
+    private checkCollision(object1 : BaseObject[], object2 : BaseObject[]) {
+        /**loop to check collision of this objects to other */
+
+
+        /**it not done yet ll be complete later */
+
+
+    }
 }
