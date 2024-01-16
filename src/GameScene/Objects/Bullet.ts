6cef79641c475761602e5b364ef64a536e38e33f
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-16 18:24:08 +0700

    add: bullet object class

--- src/GameScene/Objects/Bullet.ts
+++ src/GameScene/Objects/Bullet.ts
@@ -1,6 +1,30 @@
 import { extend } from "lodash";
 import { BaseObject } from "./BaseObject";
+import { BaseEngine } from "../Engine/BaseEngine";
 
-export class Bullet extend BaseObject {
-    
+export class Bullet extends BaseObject {
+    /** property define this bullet belong to player or bot tank */
+    private _isPlayerBullet: boolean;
+
+    /**
+     * create a bullet object with image and speed and move engine
+     * @param isPlayer is this bullet is belong to player tank
+     */
+    constructor(isPlayer: boolean) {
+        /**set id is bullet to get image of bullet */
+        super('bullet');
+
+        /**set speed of bullet */
+        this.speed = 200;
+
+        /** set move engine for bullet */
+        // this.moveEngine = new BaseEngine(false, false)
+
+        /** set property for this bullet */
+        this._isPlayerBullet = isPlayer;
+    }
+
+    update (dt) {
+        /**bullet move */
+    }
 }
