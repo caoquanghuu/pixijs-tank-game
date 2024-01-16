4ac26286df725965bac10820facaf7b49e491f7d
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-16 18:23:25 +0700

    add: control engine class

--- src/GameScene/Engine/ControlEngine.ts
+++ src/GameScene/Engine/ControlEngine.ts
@@ -1,3 +1,28 @@
+import { Direction } from "../type";
+
 export class ControlEngine {
-    
+    private _direction: Direction;
+    /**
+     * constructor to create event listener to player control
+     */
+    constructor() {
+        /** add event listener for keydown */
+    }
+
+    /**
+     * method to return direction base on player control
+     * @returns return direction
+     */
+    public getDirection(): Direction {
+        return this._direction;
+    }
+
+    private onKeyDown() {
+        /** create a direction base on key down */
+        /** assign direction to this _direction */
+    }
+
+    private onKeyUp() {
+        /**set direction to stand because key up mean that player was stop control move */
+    }
 }
