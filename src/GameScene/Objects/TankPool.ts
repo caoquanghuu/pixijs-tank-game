0df3b600e098b0a18ef08b47688aae1288940cae
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-16 18:24:27 +0700

    add: tank pool object class

--- src/GameScene/Objects/TankPool.ts
+++ src/GameScene/Objects/TankPool.ts
@@ -1,3 +1,29 @@
+import { Tank } from "./Tank";
+
 export class TankPool {
-    
+    /**a variable to limit bot tank pool */
+    private _numberOfTank: number = 20;
+    /**a array to contain bot tank */
+    private _tanksPool: Tank[] = [];
+
+    /**
+     * constructor tank pool base on number of tank
+     */
+    constructor() {
+        /**a loop to create tank and add it to tank pool */
+        for (let i = 0; i < this._numberOfTank; i++) {
+            const tank = new Tank(false);
+            this._tanksPool.push(tank);
+        }
+    }
+
+    public releaseTank() {
+        /** get tank from tank pool and return that tank */
+        // return tank;
+    }
+
+    public getTank(tank) {
+        /** get tank die from tank controller */
+        /** return tank to tank pool when tank die */
+    }
 }
