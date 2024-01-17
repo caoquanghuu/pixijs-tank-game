3737cff798180f7876942b1659d0d481c857fea8
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-17 19:41:26 +0700

    add: function to random enum key and get random arbitrary

---
+++ src/GameScene/util.ts
@@ -0,0 +1,13 @@
+export const randomEnumKey = (enumeration: any) => {
+    const keys = Object.keys(enumeration).filter(
+        (k) => !(Math.abs(Number.parseInt(k)) + 1)
+    );
+    const enumKey = keys[Math.floor(Math.random() * keys.length)];
+    return enumeration[enumKey];
+};
+
+export function getRandomArbitrary(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
