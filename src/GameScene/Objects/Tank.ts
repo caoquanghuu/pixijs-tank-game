9085b443a6fa150db684ee11aab6d6c9f905643f
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-22 17:28:46 +0700

    fix: constructor of tanks and add change texture follow direction move

--- src/GameScene/Objects/Tank.ts
+++ src/GameScene/Objects/Tank.ts
@@ -1,39 +1,46 @@
 // Tank class
 import { Point } from "@pixi/core";
-import { BaseEngine } from "../Engine/BaseEngine";
 import { RandomEngine } from "../Engine/RandomEngine";
 import { BaseObject } from "./BaseObject";
-import { Direction } from "../type";
-import { TankController } from "../Controller/TankController";
-import { methodOf } from "lodash";
+import { Direction, FireBullet } from "../type";
+import { ControlEngine } from "../Engine/ControlEngine";
+import { keyboard } from "../util";
 
 export class Tank extends BaseObject {
     /**this tank is player or bot */
     readonly _isPlayerTank: boolean;
     /**hp of this tank */
     private _HP: number;
-    private _fireBulletCallBack: Function;
+    private _fireBulletCallBack: FireBullet;
 
-    constructor(isPlayer: boolean, fireBulletCallBack: Function) {
+    constructor(isPlayer: boolean, fireBulletCallBack: FireBullet) {
         /**set image of tank is player tank or bot tank*/
-        super('tank');
-
+        super('tank-up');
         this._fireBulletCallBack = fireBulletCallBack;
-        /**set hp of tank */
-        isPlayer ? this._HP = 5 : this._HP = 1;
-
         /** set speed of tank*/
         this.speed = 100;
 
-        /** set random move engine for tank */
-        this.moveEngine = new RandomEngine();
-
         /**set move engine and type trigger fire bullet for tank base on is player or not*/
-        if(isPlayer) {
+        if (isPlayer) {
             /**add event listener to fire */
+            //set control move
+            this.moveEngine = new ControlEngine();
+            // set hp
+            this.HP = 5;
+            //control fire
+            const fire = keyboard(' ');
+            fire.press = () => {
+                this.fire(this.sprite.position, this.lastDirection, true);
+            };
         } else {
+            // set random move
+            this.moveEngine = new RandomEngine();
+            //set hp
+            this.HP = 1;
+            //change color for bot tank
+            this.sprite.tint = 'F02468';
             /** set random fire */
-            this.fire(this.sprite.position, this.lastDirection, this._isPlayerTank);
+            setInterval(() => { this.fire(this.sprite.position, this.lastDirection, false); }, 5000);
         }
 
     }
@@ -68,6 +75,27 @@ export class Tank extends BaseObject {
         }
     }
 
+    private changeTextureFollowDirection(direction: Direction) {
+        switch (direction) {
+            case Direction.UP: {
+                this.sprite = 'tank-up';
+                break;
+            }
+            case Direction.DOWN: {
+                this.sprite = 'tank-down';
+                break;
+            }
+            case Direction.RIGHT: {
+                this.sprite = 'tank-right';
+                break;
+            }
+            case Direction.LEFT: {
+                this.sprite = 'tank-left';
+                break;
+            }
+        }
+    }
+
     // private _fireCallback: (position, direction) => void = null;
 
     /**
@@ -77,9 +105,11 @@ export class Tank extends BaseObject {
     update(dt: number):void {
         // move
         this.move(dt, false);
-
+        //update new direction for random move
+        this.moveEngine.update(dt);
         // destroy
         this.destroy();
+        this.changeTextureFollowDirection(this.moveEngine.direction);
     }
 
     set HP(newHp: number) {
