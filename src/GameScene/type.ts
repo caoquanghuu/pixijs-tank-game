801f96cd32bd96aca621eae12b13ce270e645d08
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-23 20:11:54 +0700

    add: type function call back

--- src/GameScene/type.ts
+++ src/GameScene/type.ts
@@ -1,5 +1,8 @@
 import { Point } from "@pixi/core";
 import { Sprite } from "@pixi/sprite";
+import { BaseObject } from "./Objects/BaseObject";
+import { Tank } from "./Objects/Tank";
+import { Bullet } from "./Objects/Bullet";
 
 export enum Direction {
     STAND,
@@ -9,9 +12,19 @@ export enum Direction {
     RIGHT
 }
 
-export type AddToScene = (sprite : Sprite) => void;
+export type AddToScene = (sprite: Sprite) => void;
 
-export type RemoveFromScene = (srite : Sprite) => void;
+export type RemoveFromScene = (srite: Sprite) => void;
+
+export type GetObjectList = () => BaseObject[];
+
+export type GetTankList = () => Tank[];
+
+export type TankDie = (tank: Tank) => void;
+
+export type GetBulletList = () => Bullet[];
+
+export type RemoveBullet = (bullet: Bullet) => void;
 
 export type FireBullet = (position: Point, direction: Direction, isPlayerBullet: boolean) => void;
 
