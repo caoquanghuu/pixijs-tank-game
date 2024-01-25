5cd89c8100157922a619e4503a4bd82ce777c06c
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-25 21:51:36 +0700

    fix: format code and tank size, way to check tank die,

--- src/GameScene/Objects/Tank.ts
+++ src/GameScene/Objects/Tank.ts
@@ -1,74 +1,65 @@
 // Tank class
-import { Point, Rectangle } from "@pixi/core";
-import { RandomEngine } from "../Engine/RandomEngine";
-import { BaseObject } from "./BaseObject";
-import { Direction, FireBullet, TankDie } from "../type";
-import { ControlEngine } from "../Engine/ControlEngine";
-import { getRandomArbitrary, keyboard } from "../util";
-import { PositionMap } from "../Map/PositionMap";
-import { Bounds } from "@pixi/display";
+import { Point } from '@pixi/core';
+import { RandomEngine } from '../Engine/RandomEngine';
+import { BaseObject } from './BaseObject';
+import { Direction, FireBulletFn, TankDieFn } from '../type';
+import { ControlEngine } from '../Engine/ControlEngine';
+import { getRandomArbitrary, keyboard } from '../util';
 
 export class Tank extends BaseObject {
-    /**this tank is player or bot */
+
+    // this tank is player or bot
     private _isPlayerTank: boolean;
-    /**hp of this tank */
+
+    // hp of this tank
     private _HP: number;
-    private _fireBulletCallBack: FireBullet;
-    private _tankDieCall: TankDie;
-    private _positionMap: PositionMap;
-    private _keyInPositionMap: number;
+    private _fireBulletCallBack: FireBulletFn;
+    private _tankDieCall: TankDieFn;
     private _fireBulletTime: number;
 
-    constructor(isPlayer: boolean, fireBulletCallBack: FireBullet, tankDieCallBack: TankDie) {
-        /**set image of tank is player tank or bot tank*/
+    constructor(isPlayer: boolean, fireBulletCallBack: FireBulletFn, tankDieCallBack: TankDieFn) {
+
+        // set image of tank is player tank or bot tank
         super('tank-up');
         this._fireBulletCallBack = fireBulletCallBack;
         this._tankDieCall = tankDieCallBack;
-        /** set speed of tank*/
+
+        // set speed of tank
         this.speed = 100;
-        /** set size of tank */
-        this.size = { w: 20, h: 30 };
+
+        // set size of tank
+        this.size = { w: 20, h: 20 };
 
         this._isPlayerTank = isPlayer;
 
-        /**set move engine and type trigger fire bullet for tank base on is player or not*/
+        // set move engine and type trigger fire bullet for tank base on is player or not
         if (isPlayer) {
-            /**add event listener to fire */
+
             //set control move
             this.moveEngine = new ControlEngine();
+
             // set hp
             this.HP = 5;
-            /** create new position map to get key */
-            const positionMap = new PositionMap();
-            /** assign key in position map*/
-            this._keyInPositionMap = positionMap.keyInArrayRectangle;
-            /** create a new rectangle of tank */
-            this.rectangle = PositionMap.createNewPosition(this.size);
-            /** create a random available position */
-            this.sprite.position.set(this.rectangle.x, this.rectangle.y);
-            //control fire
+
+            // set control fire
             const fire = keyboard(' ');
             fire.press = () => {
-                this.fire(this.sprite.position, this.lastDirection, true);
+                this.fire(this.position, this.lastDirection, true);
             };
         } else {
+
             // set random move
             this.moveEngine = new RandomEngine();
+
             //set hp
             this.HP = 1;
+
             //change color for bot tank
             this.sprite.tint = 'F02468';
+
             // set time for fire bullet
             this._fireBulletTime = 5000;
         }
-    }
-
-    get keyInPositionMap(): number {
-        return this._keyInPositionMap;
-    }
-
-    set keyInPositionMap(key: number) {
-        this._keyInPositionMap = key;
     }
 
     /**
@@ -76,31 +67,18 @@ export class Tank extends BaseObject {
      */
     public fire(position: Point, direction: Direction, isPlayer: boolean) {
 
+        // call fire bullet to tank controller
         this._fireBulletCallBack(position, direction, isPlayer);
-        // fire bullet
-        // call fire bullet from controller
-        // this._fireBulletCallBack(this.sprite.position, this.lastDirection)
-        //
-        // this._fireCallback?.(this.sprite.position, this.moveEngine.direction);
-    }
-
-    /**
-     * tank has been hit by the bullet
-     */
-    public onHit() {
-        //reduce HP
-        this._HP -= 1;
-        if (this._HP === 0) {
-            this.destroy();
-        }
     }
 
     /**
      *  tank hp reduce to 0 and tank will be destroy
      */
     public destroy() {
-        // call tank die to tank controller
-        this._tankDieCall(this);
+        if (this.HP === 0) {
+            // call tank die to tank controller
+            this._tankDieCall(this);
+        }
     }
     /**
      * change texture and size of tank when direction of tank change
@@ -110,54 +88,57 @@ export class Tank extends BaseObject {
         switch (direction) {
             case Direction.UP: {
                 this.sprite = 'tank-up';
-                this.size = { w: 20, h: 30 };
                 break;
             }
             case Direction.DOWN: {
                 this.sprite = 'tank-down';
-                this.size = { w: 20, h: 30 };
                 break;
             }
             case Direction.RIGHT: {
                 this.sprite = 'tank-right';
-                this.size = { w: 35, h: 20 };
                 break;
             }
             case Direction.LEFT: {
                 this.sprite = 'tank-left';
-                this.size = { w: 35, h: 20 };
                 break;
             }
         }
     }
-
-    // private _fireCallback: (position, direction) => void = null;
 
     /**
      * Update tank
      * @param dt delta time from ticker
      */
     update(dt: number): void {
+
         // move
         this.move(dt, false);
+
+        // check hp tank and destroy it if hp = 0
+        this.destroy();
+
         //update new direction for random move
         this.moveEngine.update(dt);
+
         // change texture when direction of tank change
         this.changeTextureFollowDirection(this.moveEngine.direction);
-        // push and update position of tank in position map
+
         // set fire bullet call for bot tank
         if (!this._isPlayerTank) {
             this._fireBulletTime -= dt;
             if (this._fireBulletTime <= 0) {
-                this.fire(this.sprite.position, this.lastDirection, false);
+                this.fire(this.position, this.lastDirection, false);
                 this._fireBulletTime = getRandomArbitrary(3000, 5000);
             }
         }
-        PositionMap.setPositionMap(this.rectangle, this._keyInPositionMap);
     }
 
     set HP(newHp: number) {
         this._HP = newHp;
+    }
+
+    get HP(): number {
+        return this._HP;
     }
 
     get isPlayerTank(): boolean {
