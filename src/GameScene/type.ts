6134c4d3181b8db88f3dbda5f9376f6062acbe45
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-01-25 21:42:28 +0700

    fix: rename function type name

--- src/GameScene/type.ts
+++ src/GameScene/type.ts
@@ -1,8 +1,9 @@
-import { Point } from "@pixi/core";
-import { Sprite } from "@pixi/sprite";
-import { BaseObject } from "./Objects/BaseObject";
-import { Tank } from "./Objects/Tank";
-import { Bullet } from "./Objects/Bullet";
+/* eslint-disable no-unused-vars */
+import { Point, Rectangle } from '@pixi/core';
+import { Sprite } from '@pixi/sprite';
+import { BaseObject } from './Objects/BaseObject';
+import { Tank } from './Objects/Tank';
+import { Bullet } from './Objects/Bullet';
 
 export enum Direction {
     STAND,
@@ -15,23 +16,25 @@ export enum Direction {
 export type Size = {
     w: number,
     h: number,
-}
+};
 
-export type AddToScene = (sprite: Sprite) => void;
+export type AddToSceneFn = (sprite: Sprite) => void;
 
-export type RemoveFromScene = (srite: Sprite) => void;
+export type RemoveFromSceneFn = (srite: Sprite) => void;
 
-export type GetObjectList = () => BaseObject[];
+export type GetObjectListFn = () => BaseObject[];
 
-export type GetTankList = () => Tank[];
+export type GetTankListFn = () => Tank[];
 
-export type TankDie = (tank: Tank) => void;
+export type TankDieFn = (tank: Tank) => void;
 
-export type HandleTankMove = (tank: Tank) => void;
+export type HandleTankMoveFn = (tank: Tank) => void;
 
-export type GetBulletList = () => Bullet[];
+export type GetBulletListFn = () => Bullet[];
 
-export type RemoveBullet = (bullet: Bullet) => void;
+export type RemoveBulletFn = (bullet: Bullet) => void;
 
-export type FireBullet = (position: Point, direction: Direction, isPlayerBullet: boolean) => void;
+export type FireBulletFn = (position: Point, direction: Direction, isPlayerBullet: boolean) => void;
+
+export type CreateNewRandomPositionFn = (size: Size) => Rectangle;
 
